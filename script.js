abddffe9d5c7657d736705a845737aeaa73df154
Madiba Desree <107559625+desreedimaky@users.noreply.github.com>
2024-10-16 14:20:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript to toggle the navbar on smaller screens
+document.getElementById('navbarToggle').addEventListener('click', function () {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+});
+
